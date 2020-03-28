0ababdf081fcd570a8e3784b51c9823b32b955d4
Vadim A. Nikulin <accs@vnik.me>
2020-03-28 22:21:45 +0000

    Intersection points.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 
 
+constexpr double EPS = 1e-7;
+
 struct TPoint {
     double X = 0.0, Y = 0.0;
     TPoint() = default;
@@ -18,10 +20,10 @@ struct TSphere {
     TSphere() = default;
     TSphere(const TPoint &position, double direction, double velocity)
         : Position(position), Direction(direction), Velocity(velocity) {
-        int d = int(Direction / 360);
-        Direction -= d * 360;
+        int d = int(Direction / 2 / M_PI);
+        Direction -= d * (2 * M_PI);
         if (Direction < 0.0)
-            Direction += 360;
+            Direction += 2 * M_PI;
     }
 };
 
@@ -32,15 +34,61 @@ int PointsOfIntersectionOfTwoSpheres(const TSphere &s0, const TSphere &s1, doubl
     double a = u0 * u0 + u1 * u1 + v0 * v0 + v1 * v1 - 2 * u0 * u1 - 2 * v0 * v1;
     double b = 2 * x0 * u0 - 2 * x0 * u1 - 2 * u0 * x1 + 2 * x1 * u1 + 2 * y0 * v0 - 2 * y0 * v1 - 2 * v0 * y1 + 2 * y1 * v1;
     double c = x0 * x0 + x1 * x1  + y0 * y0 + y1 * y1 - 2 * x0 * x1 - 2 * y0 * y1 - 4 * r * r;
-    std::cout << a << ' ' << b << ' ' << c << std::endl;
-    return 0;
+    //std::cout << a << ' '<< b << ' ' << c << std::endl;
+    if (fabs(a) < EPS) {
+        if (fabs(b) < EPS) {
+            if (fabs(c) < EPS)
+                return 1;
+            else
+                return 0;
+        } else {
+            t0 = -c / b;
+            return 1;
+        }
+    }
+    double d2 = b * b - 4 * a * c;
+    //std::cout << d2 << std::endl;
+    if (d2 < -EPS)
+        return 0;
+    if (d2 < EPS) {
+        t0 = -b / 2 / a;
+        return 1;
+    }
+    double d = sqrt(d2);
+    //std::cout << d << std::endl;
+    t0 = (-b - d) / 2 / a;
+    t1 = (-b + d) / 2 / a;
+    return 2;
 }
 
-int main() {
+bool TestIntersection1() {
     TSphere s0(TPoint(0, 0), 0, 0);
     TSphere s1(TPoint(0, 0), 0, 1);
     double t0, t1;
-    PointsOfIntersectionOfTwoSpheres(s0, s1, 1, t0, t1);
+    int points = PointsOfIntersectionOfTwoSpheres(s0, s1, 1, t0, t1);
+    if (points != 2)
+        return false;
+    if (fabs(t0 + 2) > EPS || fabs(t1 - 2) > EPS)
+        return false;
+    return true;
+}
+
+bool TestIntersection2() {
+    TSphere s0(TPoint(-2, 0), M_PI / 4, 1);
+    TSphere s1(TPoint(2, 0), M_PI * 3 / 4, 1);
+    double t0, t1;
+    int points = PointsOfIntersectionOfTwoSpheres(s0, s1, 1, t0, t1);
+    std::cout << points << ' ' << t0 << ' ' << t1 << std::endl;
+    if (points != 2)
+        return false;
+    if (fabs(t0 + 2) > EPS || fabs(t1 - 2) > EPS)
+        return false;
+    return true;
+}
+
+int main() {
+    std::cout << TestIntersection1() << std::endl;
+    std::cout << TestIntersection2() << std::endl;
     return 0;
 }
 
